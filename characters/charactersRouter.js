e93e806bb80dc8f41d2dca21546134e0fa3c91b8
das41236 <davsoudry@gmail.com>
2018-03-15 17:27:51 -0500

    Find films, find charbyid, populate homeworld complete

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,27 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+// router.get('/', (req, res) => {
+//     Character.find({})
+//     .then(char => {
+//         res.json(char);
+//     })
+//     .catch(err => {
+//         res.status(500).json({ error: 'error retrieving characters'})
+//     })
+// })
 
+router.get('/:id', function(req, res) {
+    const { id } = req.params;
+
+    Character.findById(id )
+    .populate('homeworld')
+    .populate('movies', 'title')
+    .then(char => {
+        res.json(char)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'There was an error retrieving character'});
+    })
+});
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,18 +6,26 @@ const router = express.Router();
 
 // add endpoints here
 router.get('/', function(req, res) {
-    const producerFilter = req.query.producer;
+    const producerFilter = new RegExp(req.query.producer, 'i');
+    const releaseDateFilter = new RegExp(req.query.released, );
 
     let query = Film.find({})
     .sort('episode')
     // .populate('characters planets species')
     // .populate('characters', 'name gender height skin_color')
     // .populate('planets', 'name climate terrain gravity diameter')
-    .select('title producer')
+    // .select('title producer release_date')
 
-    if (producerFilter) {
-        query.where({ producer: /gary kurtz/i });
+    if (req.query.producer) {
+        query.where({ producer: producerFilter })
+        .select('title release_date producer')
+    } else if (req.query.released) {
+        query.where({ release_date: releaseDateFilter })
+        .select('title release_date')
     }
+    // if (releaseDateFilter) {
+    //     query.where({ released: /2005/i });
+    // }
     query.then(films => {
         res.json(films);
     });
